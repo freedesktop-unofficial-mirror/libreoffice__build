8d719efb6c7ef2bdd1b91f3c57d3f90e893e8a67
nobody <nobody@gnome.org>
2005-10-25 13:01:52 +0000

    This commit was manufactured by cvs2svn to create tag 'NOEL_BEFORE_NAMESPACE_CHANGES'.

--- scratch/mono-uno-bootstrap/assembly.cs
+++
@@ -1,4 +0,0 @@
-[assembly:System.Reflection.AssemblyProduct( "CLI-UNO Language Binding" )]
-[assembly:System.Reflection.AssemblyDescription( "CLI-UNO Helper Library" )]
-[assembly:System.Reflection.AssemblyDelaySign(false)]
-[assembly:System.Reflection.AssemblyCompany( "OpenOffice.org" )]
