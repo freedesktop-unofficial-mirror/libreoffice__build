428436ce5498bac66dcb2edab4196629aa969008
nobody <nobody@gnome.org>
2006-01-09 13:35:17 +0000

    This commit was manufactured by cvs2svn to create tag 'KOHEI_IMPORT_DONE'.

--- bonobo/src/remote-uno-helper.h
+++
@@ -1,9 +0,0 @@
-#ifndef REMOTE_UNO_HELPER_H
-#define REMOTE_UNO_HELPER_H
-
-#include <com/sun/star/uno/XComponentContext.hpp>
-
-::com::sun::star::uno::Reference<
-		::com::sun::star::uno::XComponentContext > getComponentContext();
-
-#endif
