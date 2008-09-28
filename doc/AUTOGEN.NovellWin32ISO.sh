b37d4acd60dac1bd82720e7558fd74ac2bc8b0df
Tor Lillqvist <tml@novell.com>
2008-09-28 16:53:36 +0000

    Add --with-extension-integration.
    * doc/AUTOGEN.NovellWin32ISO.sh: Add --with-extension-integration.

--- doc/AUTOGEN.NovellWin32ISO.sh
+++ doc/AUTOGEN.NovellWin32ISO.sh
@@ -1,2 +1,2 @@
 NOCONFIGURE=indeed ./autogen.sh
-./configure --with-distro=NovellWin32ISO --with-lang="af ar bn-IN ca cs da de el en-GB es fi fr gu-IN he hi-IN hu it ja ko mr-IN nb nl nn pl pt-BR pt ru sk sv ta-IN xh zh-CN zh-TW zu" --with-poor-help-localizations="af ar bn-IN ca el en-GB fi gu-IN hi-IN mr-IN nb nn pt sk ta-IN xh zu" --with-dejavu-fonts=2.24 --disable-build-mozilla
+./configure --with-distro=NovellWin32 --with-lang="af ar bn-IN ca cs da de el en-GB es fi fr gu-IN he hi-IN hu it ja ko mr-IN nb nl nn pl pt-BR pt ru sk sv ta-IN xh zh-CN zh-TW zu" --with-poor-help-localizations="af ar bn-IN ca el en-GB fi gu-IN hi-IN mr-IN nb nn pt sk ta-IN xh zu" --with-dejavu-fonts=2.24 --disable-build-mozilla --with-extension-integration
