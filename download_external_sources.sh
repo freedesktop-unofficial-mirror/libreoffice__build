a07c06902f8dca878109d3fdfbebca23ec718dc5
Rene Engelhard <rene@debian.org>
2010-05-14 13:07:40 +0200

    download external upstream sources in download
    * bin/build-ooo:
    * bin/setup.in:
    * download.in:
    * download_external_sources.sh:

---
+++ download_external_sources.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. ./bin/setup
+
+export TARFILE_LOCATION
+TAG=`echo $CVSTAG | tr a-z A-Z | sed -e s/-M/_m/`
+MWS=`echo $TAG | cut -d_ -f1`
+tmp=`mktemp -q -d`
+
+cd $tmp
+wget http://hg.services.openoffice.org/${MWS}/raw-file/${TAG}/fetch_tarballs.sh && chmod 755 ./fetch_tarballs.sh
+wget http://hg.services.openoffice.org/${MWS}/raw-file/${TAG}/ooo.lst
+./fetch_tarballs.sh ooo.lst &&  rm -f fetch_tarballs.sh ooo.lst
+cd ..
+rmdir $tmp
+
