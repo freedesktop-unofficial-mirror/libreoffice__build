b1d7dcdfc64592d92d7eafe37793a0dd05898cee
Miklos Vajna <vmiklos@frugalware.org>
2010-10-23 18:13:00 +0200

    find-german-comments: simple hack to find german comments in the source code

---
+++ scratch/german-comments/t/test.cxx
@@ -0,0 +1,59 @@
+before_comment();
+foo(); // single line comment
+//single line comment 2
+after_comment();
+
+/*
+ * If there was some unconverted bytes on the last cycle then they
+ */
+
+not_a_comment();
+/*
+ * dann mal einen harten Seitenumbruch einfuegen
+ */
+
+not_a_comment();
+
+/**************************************************************************/
+
+// #110680#
+
+    DEFAULTFONT_SERIF,          // FNT_VARIABLE
+
+// Set base URI
+
+/*************************************************************************
+|*
+|* Deinitialisierung
+|*
+\************************************************************************/
+
+/*************************************************************************
+|*
+|* Deinitialising
+|*
+\************************************************************************/
+
+/* dann mal einen harten Seitenumbruch einfuegen */
+
+    // used to convert the 4 special ExtraProg/UINames for
+    // RES_POOLCOLL_LABEL_DRAWING,  RES_POOLCOLL_LABEL_ABB,
+    // RES_POOLCOLL_LABEL_TABLE, RES_POOLCOLL_LABEL_FRAME
+    // forth and back.
+    // Non-matching names remain unchanged.
+
+        bCntntCheck( FALSE ), // --> FME 2005-05-13 #i43742# <--
+        bInFrontOfLabel( FALSE ), // #i27615#
+        bInNumPortion(FALSE), // #i23726#
+        nInNumPostionOffset(0) // #i23726#
+
+////////////////////////////////////////////////////////////
+//
+//      SmFontPickListBox
+//
+
+// --------------------
+// SwGrfNode
+// --------------------
+
+//#define WID_???                               1024
