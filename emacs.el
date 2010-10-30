8136be0bb5913abb64f0f4dc08667029a4cf0c95
Tim Retout <tim@retout.co.uk>
2010-10-30 10:59:59 +0100

    emacs.el: Replace all mentions of OOo with LO.
    Signed-off-by: Tim Retout <tim@retout.co.uk>

--- emacs.el
+++ emacs.el
@@ -1,34 +1,34 @@
-(defvar openoffice.org-path-regexp
-  "openoffice"
-  "Regexp that matches paths into your OpenOffice.org source tree.")
+(defvar libreoffice-path-regexp
+  "libreoffice"
+  "Regexp that matches paths into your LibreOffice source tree.")
 
-(defun openoffice.org-c-hook ()
-  "Set C++ style and tab stopsto match OOo style."
-  (when (string-match openoffice.org-path-regexp buffer-file-name)
-      (message "Using OpenOffice.org code settings")
+(defun libreoffice-c-hook ()
+  "Set C++ style and tab stops to match LO style."
+  (when (string-match libreoffice-path-regexp buffer-file-name)
+      (message "Using LibreOffice code settings")
       (setq tab-width 4)
       (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72
 			    76 80 84 88 92 96 100 104 108 112 116 120))
       (c-set-style "stroustrup")
       (set-variable 'c-basic-offset 4)
       (set-variable 'indent-tabs-mode nil)
-      (let ((env-file (openoffice.org-set-env-file buffer-file-name)))
+      (let ((env-file (libreoffice-set-env-file buffer-file-name)))
         (setq compile-command (if (null env-file)
                                   "dmake"
                                 (concat ". "
                                         (file-relative-name env-file)
                                         "; dmake"))))))
 
-(add-hook 'c-mode-common-hook 'openoffice.org-c-hook)
+(add-hook 'c-mode-common-hook 'libreoffice-c-hook)
 
 (add-to-list 'auto-mode-alist (cons "\\.sdi\\'" 'c++-mode))
 (add-to-list 'auto-mode-alist (cons "\\.hrc\\'" 'c++-mode))
 (add-to-list 'auto-mode-alist (cons "\\.src\\'" 'c++-mode))
 
 ;; stolen from vc-hooks.el (vc-find-root)
-(defun openoffice.org-src-dir (file)
-  "Find the root of an OpenOffice.org source directory.
-Find the root of the OpenOffice.org source directory that
+(defun libreoffice-src-dir (file)
+  "Find the root of an LibreOffice source directory.
+Find the root of the LibreOffice source directory that
 contains FILE, or nil if FILE is not in such a directory"
   (let ((dir nil))
     (while (not (or dir
@@ -39,11 +39,11 @@ contains FILE, or nil if FILE is not in such a directory"
         (setq file (directory-file-name file))))
     dir))
 
-(defun openoffice.org-set-env-file (file)
-  "Find OOo *Env.Set.sh script.
+(defun libreoffice-set-env-file (file)
+  "Find LO *Env.Set.sh script.
 Returns the name of the solar environment setter script for the
-OpenOffice.org source tree to which FILE belongs. Unix-only."
-  (let* ((root (openoffice.org-src-dir file))
+LibreOffice source tree to which FILE belongs. Unix-only."
+  (let* ((root (libreoffice-src-dir file))
         (files (and root
                     (file-expand-wildcards
                      (expand-file-name "*Env.Set.sh" root)))))
