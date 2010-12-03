adb2c4f2792c3f2c8162aad4eb88a1bf84e8fcd8
Miklos Vajna <vmiklos@frugalware.org>
2010-12-03 13:13:00 +0100

    find-undocumented-classes: allow checking a few files only
    Previously the current directory was checked (recursively), now in case
    additional parameters present, then they will be handled as file or
    directory names and those will be checked only instead of the whole
    current directory.

--- scratch/find-undocumented-classes.sh
+++ scratch/find-undocumented-classes.sh
@@ -7,6 +7,7 @@ quiet=n
 if [ "$1" = "-q" ]; then
     filter=">/dev/null"
     quiet=y
+    shift
 fi
 
 doxygen=$(mktemp -d)
@@ -14,13 +15,17 @@ eval doxygen -g $doxygen/doxygen.cfg $filter
 sed -i "/HTML_OUTPUT/s|html|$doxygen/html|" $doxygen/doxygen.cfg
 sed -i '/GENERATE_LATEX/s/= YES/= NO/' $doxygen/doxygen.cfg
 sed -i '/RECURSIVE/s/= NO/= YES/' $doxygen/doxygen.cfg
+# do we have any arguments?
+if [ -n "$*" ]; then
+    sed -i "/^INPUT[^_]/s|=.*|= $*|" $doxygen/doxygen.cfg
+fi
 eval doxygen $doxygen/doxygen.cfg $filter 2> $doxygen/errors.txt
 if [ "$quiet" == "n" ]; then
     echo
     echo "The following classes are undocumented:"
     echo
 fi
-cat $doxygen/errors.txt|grep 'Warning: Compound.*is not documented'
+cat $doxygen/errors.txt|grep -i 'Warning: Compound.*is not documented'
 rm -rf $doxygen
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab:
