c44442e0586c9c94c5c57f67dd7eb4b2d4bcde85
Miklos Vajna <vmiklos@frugalware.org>
2010-12-27 13:57:38 +0100

    Use OOO_SOURCEDIRNAME directly when downloading external sources
    This is needed because of the LIBREOFFICE_3_3_0_2 -> libreoffice-3.3.0.2
    tag name change.
    
    Signed-off-by: Jan Holesovsky <kendy@suse.cz>

--- download_external_sources.sh
+++ download_external_sources.sh
@@ -8,12 +8,11 @@ export CLONEDIR
 if test -e $CLONEDIR/bootstrap/fetch_tarballs.sh -a -e $CLONEDIR/bootstrap/ooo.lst; then
     $CLONEDIR/bootstrap/fetch_tarballs.sh $CLONEDIR/bootstrap/ooo.lst
 else
-    GIT_TAG=`echo $OOO_SOURCEDIRNAME | tr "a-z.-" "A-Z__"`
     tmp=`mktemp -q -d`
 
     cd $tmp
-    wget http://cgit.freedesktop.org/libreoffice/bootstrap/plain/fetch_tarballs.sh?id=$GIT_TAG -O fetch_tarballs.sh && chmod 755 fetch_tarballs.sh
-    wget http://cgit.freedesktop.org/libreoffice/bootstrap/plain/ooo.lst?id=$GIT_TAG -O ooo.lst
+    wget http://cgit.freedesktop.org/libreoffice/bootstrap/plain/fetch_tarballs.sh?id=$GITTAG -O fetch_tarballs.sh && chmod 755 fetch_tarballs.sh
+    wget http://cgit.freedesktop.org/libreoffice/bootstrap/plain/ooo.lst?id=$GITTAG -O ooo.lst
     ./fetch_tarballs.sh ooo.lst &&  rm -f fetch_tarballs.sh ooo.lst
     cd ..
     rmdir $tmp
