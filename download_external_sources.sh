53fcd05a7bd914bd3222ce1893129b8c7aee7b67
Miklos Vajna <vmiklos@frugalware.org>
2010-12-27 13:57:38 +0100

    Use OOO_SOURCEDIRNAME directly when downloading external sources
    This is needed because of the LIBREOFFICE_3_3_0_2 -> libreoffice-3.3.0.2
    tag name change.

--- download_external_sources.sh
+++ download_external_sources.sh
@@ -11,12 +11,11 @@ if test -e bootstrap/download -a -e bootstrap/ooo.lst; then
         ./download ooo.lst
     )
 else
-    GIT_TAG=`echo $OOO_SOURCEDIRNAME | tr "a-z.-" "A-Z__"`
     tmp=`mktemp -q -d`
 
     cd $tmp
-    wget http://cgit.freedesktop.org/libreoffice/bootstrap/plain/fetch_tarballs.sh?id=$GIT_TAG -O fetch_tarballs.sh && chmod 755 fetch_tarballs.sh
-    wget http://cgit.freedesktop.org/libreoffice/bootstrap/plain/ooo.lst?id=$GIT_TAG -O ooo.lst
+    wget http://cgit.freedesktop.org/libreoffice/bootstrap/plain/fetch_tarballs.sh?id=$OOO_SOURCEDIRNAME -O fetch_tarballs.sh && chmod 755 fetch_tarballs.sh
+    wget http://cgit.freedesktop.org/libreoffice/bootstrap/plain/ooo.lst?id=$OOO_SOURCEDIRNAME -O ooo.lst
     ./fetch_tarballs.sh ooo.lst &&  rm -f fetch_tarballs.sh ooo.lst
     cd ..
     rmdir $tmp
