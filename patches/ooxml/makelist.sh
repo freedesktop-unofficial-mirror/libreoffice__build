6adb0f6d7142e7cee643f604a6ff502ebec810e7
Petr Mladek <pmladek@suse.cz>
2011-04-27 17:51:58 +0200

    pushed buildfix-oox-depends-on-unotools.diff into git

--- patches/ooxml/makelist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ls -l | grep -E "\.diff$" | sed \
-    -e "s/.*\ oox/oox/g" \
-    -e "s/.*\ xmlfilter/xmlfilter/g" \
-    -e "s/.*\ writerfilter/writerfilter/g" \
-    -e "s/.*\ ooxml/ooxml/g" \
-    | sort > list.txt
